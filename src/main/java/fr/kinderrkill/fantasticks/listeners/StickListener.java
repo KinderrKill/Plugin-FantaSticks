d85bd28cbeb0a4bfa1a2acb04a401dc08d397dca
Kinderrkill <sauvage-etienne@orange.fr>
2021-10-20 20:59:08 +0200

    Add listeners test

--- src/main/java/fr/kinderrkill/fantasticks/listeners/StickListener.java
+++ src/main/java/fr/kinderrkill/fantasticks/listeners/StickListener.java
@@ -1,11 +1,15 @@
 package fr.kinderrkill.fantasticks.listeners;
 
 import fr.kinderrkill.fantasticks.FantaSticks;
+import org.bukkit.Material;
+import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
 
 public class StickListener implements Listener {
 
@@ -20,7 +24,23 @@ public class StickListener implements Listener {
         Player player = event.getPlayer();
         ItemStack item = player.getInventory().getItemInMainHand();
 
-        player.sendMessage("Using " + item.getType() + " item with action " + event.getAction() + " (" + event.getPlayer().isSneaking() + ")");
+        if (item.getType() == Material.STICK) {
+            Fireball fireball = player.launchProjectile(Fireball.class);
+            fireball.setIsIncendiary(false);
+            fireball.setBounce(false);
+            fireball.setMetadata("damageBlock", new FixedMetadataValue(plugin, false));
+        }
+    }
+
+    @EventHandler
+    public void onProjectileExplode(EntityExplodeEvent event) {
+        if (event.getEntity() instanceof Fireball) {
+            Fireball fireball = (Fireball) event.getEntity();
+            if (fireball.getShooter() instanceof Player) {
+                event.getEntity().getWorld().createExplosion(event.getLocation(), 1f, false, !fireball.hasMetadata("damageBlock"));
+                event.setCancelled(true);
+            }
+        }
     }
 
 }
